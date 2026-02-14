0c5f095c9c2f5db13e2abe50b06d323379708646
Neven Sajko <4944410+nsajko@users.noreply.github.com>
2026-02-14 22:03:13 +0100

    add some tests (#3)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,39 @@
 using LightBoundsErrors
 using Test
-using Aqua
+
+function error_message_pattern(indices::String, collection)
+    t = typeof(collection)
+    a = axes(collection)
+    ["LightBoundsError: ", "out-of-bounds index", "`collection[$indices]`", "`typeof(collection) == $t`", "`axes(collection) == $a`"]
+end
 
 @testset "LightBoundsErrors.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(LightBoundsErrors)
+    @testset "`LightBoundsError`" begin
+        @test LightBoundsError <: Exception
     end
-    # Write your tests here.
+    @testset "`checkbounds_lightboundserror`" begin
+        elt = Float32
+        @testset "0D" begin
+            a = Array{elt, 0}(undef)
+            for n ∈ 0:4
+                @test let is = ntuple(Returns(1), n)
+                    nothing === @inferred checkbounds_lightboundserror(a, is...)
+                end
+            end
+            for i ∈ [-1, 0, 2]
+                @test_throws LightBoundsError checkbounds_lightboundserror(a, i)
+                @test_throws error_message_pattern("$i", a) checkbounds_lightboundserror(a, i)
+                for j ∈ -1:2
+                    @test_throws LightBoundsError checkbounds_lightboundserror(a, i, j)
+                    @test_throws error_message_pattern("$i, $j", a) checkbounds_lightboundserror(a, i, j)
+                end
+            end
+        end
+    end
+end
+
+using Aqua: Aqua
+
+@testset "Code quality (Aqua.jl)" begin
+    Aqua.test_all(LightBoundsErrors)
 end
