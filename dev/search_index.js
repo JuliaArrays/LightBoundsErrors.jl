cf8ab4933bfe174842e54dfd81a4d7d9814d1fa7
Documenter.jl <documenter@juliadocs.github.io>
2026-02-14 20:05:30 +0000

    build based on 3151fd9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#LightBoundsErrors","page":"Home","title":"LightBoundsErrors","text":"Documentation for LightBoundsErrors.\n\n","category":"section"}]
+}
